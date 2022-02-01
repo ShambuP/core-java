b43b4a3cd9a01d011c92706008ef917a3be8dec3
Shambup <shambupuranikamath.xworkz@gmail.com>
2022-02-01 10:38:19 +0530

    metro market

---
+++ MetroMarket.java
@@ -0,0 +1,21 @@
+class MetroMarket{
+
+
+
+  public static void main(String a[]){
+  
+  String clothsForMen[] = {"shirts ", "pants","jeans","hoodies"};
+  String clothsForWomen[] ={"choodies","sarees", "jackets"};
+  
+  for (int i=0; i<clothes.lengtth; i++){
+  
+  System.out.println("the clothes for men are " +clothesFOrMen[i] );
+  
+  }
+  }
+
+
+
+
+
+}
