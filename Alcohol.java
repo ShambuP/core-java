d7149d9a8bc6cda352dc40bc58531e0201b3cb93
Shambup <shambupuranikamath.xworkz@gmail.com>
2022-02-01 11:07:33 +0530

    committing varriables

---
+++ Alcohol.java
@@ -0,0 +1,13 @@
+class Alcohol{
+public static void main(String a[]){
+
+String name = "brocode";
+int    price =  150;
+System.out.println(" the name of alcohol is "+ name);
+System.out.println(" the price is "+ price);
+
+
+}
+
+
+}
